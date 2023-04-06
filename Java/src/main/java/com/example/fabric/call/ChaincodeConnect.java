1103e26e8ae09fad62dd377fbb54488c3b76b76f
Happier0120 <1982863351@qq.com>
2023-04-07 01:04:51 +0800

    secone commit

--- Java/src/main/java/com/example/fabric/call/ChaincodeConnect.java
+++ Java/src/main/java/com/example/fabric/call/ChaincodeConnect.java
@@ -23,11 +23,11 @@ public class ChaincodeConnect {
     // helper function for getting connected to the gateway
     public static Gateway connect() throws Exception {
         // 创建钱包，加载用户密钥
-        Path walletPath = Paths.get("/home", "ccccit", "crypto-config", "wallet");
+        Path walletPath = Paths.get("/home", "sweeney", "web3j", "fabric-samples", "test-network", "crypto-config", "wallet");
         Wallet wallet = Wallets.newFileSystemWallet(walletPath);
         populateWallet(wallet);
         // 网络连接，加载fabric信息与peer节点密钥
-        Path networkConfigPath = Paths.get("/home", "ccccit", "crypto-config", "connection.yaml");
+        Path networkConfigPath = Paths.get("/home", "sweeney", "web3j", "fabric-samples", "test-network", "crypto-config", "connection.yaml");
         // 连接建立，后续可用于执行链码
         Gateway.Builder builder = Gateway.createBuilder();
         builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
@@ -35,15 +35,15 @@ public class ChaincodeConnect {
     }
 
     public static void populateWallet(Wallet wallet) throws InvalidKeyException, CertificateException, IOException {
-        String certPathAdmin = "/home/ccccit/crypto-config/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".blockchain.com/ca/ca." + Submit.users.get(Submit.currentUser) + ".blockchain.com-cert.pem";
-        String keyPathAdmin = "/home/ccccit/crypto-config/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".blockchain.com/ca/priv_sk";
+        String certPathAdmin = "/home/sweeney/web3j/fabric-samples/test-network/organizations/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".example.com/ca/ca." + Submit.users.get(Submit.currentUser) + ".example.com-cert.pem";
+        String keyPathAdmin = "/home/sweeney/web3j/fabric-samples/test-network/organizations/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".example.com/ca/priv_sk";
         X509Certificate certAdmin = Identities.readX509Certificate(new PemReader(new FileReader(certPathAdmin)));
         PrivateKey keyAdmin = Identities.readPrivateKey(new PemReader(new FileReader(keyPathAdmin)));
         X509Identity identityAdmin = Identities.newX509Identity(Submit.users.get(Submit.currentUser), certAdmin, keyAdmin);
         wallet.put("admin", identityAdmin);
 
-        String certPath = "/home/ccccit/crypto-config/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".blockchain.com/users/User1@" + Submit.users.get(Submit.currentUser) + ".blockchain.com/msp/signcerts/User1@" + Submit.users.get(Submit.currentUser) + ".blockchain.com-cert.pem";
-        String keyPath = "/home/ccccit/crypto-config/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".blockchain.com/users/User1@" + Submit.users.get(Submit.currentUser) + ".blockchain.com/msp/keystore/priv_sk";
+        String certPath = "/home/sweeney/web3j/fabric-samples/test-network/organizations/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".example.com/users/User1@" + Submit.users.get(Submit.currentUser) + ".example.com/msp/signcerts/User1@" + Submit.users.get(Submit.currentUser) + ".example.com-cert.pem";
+        String keyPath = "/home/sweeney/web3j/fabric-samples/test-network/organizations/peerOrganizations/" + Submit.users.get(Submit.currentUser) + ".example.com/users/User1@" + Submit.users.get(Submit.currentUser) + ".example.com/msp/keystore/priv_sk";
         X509Certificate cert = Identities.readX509Certificate(new PemReader(new FileReader(certPath)));
         PrivateKey key = Identities.readPrivateKey(new PemReader(new FileReader(keyPath)));
         X509Identity identity = Identities.newX509Identity(Submit.users.get(Submit.currentUser), cert, key);
